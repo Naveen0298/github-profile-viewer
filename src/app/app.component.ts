499cf8aca7765ba1f3c4364fa837ce460e273cdf
naveen0298 <naveenpolisetty0298@gmail.com>
2019-11-15 18:16:40 +0530

    created github-profile-viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'github-profile-viewer';
+  name: any;
+  src: any;
+  data: any;
+  location: any;
+  searchBox: FormGroup;
+  dataLoading: any;
+  noDatafound: any;
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar) { }
+  ngOnInit() {
+    this.searchBox = new FormGroup({
+      searchText: new FormControl('', Validators.required)
+    });
+  }
+  onSubmit(searchBox: FormGroup) {
+    const userText = this.searchBox.value.searchText;
+    const storeData = localStorage.getItem(userText);
+    this.dataLoading = true;
+    if (storeData) {
+      this.data = JSON.parse(storeData);
+      this.fillDetails();
+    } else {
+      this.addUserdetails(userText).subscribe(response => {
+        this.data = response;
+        localStorage.setItem(userText, JSON.stringify(this.data));
+        this.fillDetails();
+        console.log(this.data);
+      },
+        err => {
+          this.noDatafound = true;
+          this.data = '';
+          this.dataLoading = false;
+          this.openSnackBar('Oops!', 'close');
+        });
+
+    }
+  }
+  fillDetails() {
+    this.dataLoading = false;
+    this.noDatafound = false;
+    this.name = this.data.name;
+    this.src = this.data.avatar_url;
+    this.location = this.data.location;
+  }
+  addUserdetails(userName) {
+    return this.http.get('https://api.github.com/users/' + userName + '?access_token=beba3c150021bfb49769385927dfa59fac2cdf04');
+  }
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
 }
